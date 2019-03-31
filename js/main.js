cd252a2a430c65e214cd61f8cf518eae5b074731
BrannanC <brannanconrad@gmail.com>
2019-03-31 18:43:43 -0500

    added some responsivness to carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const tablet = window.matchMedia( "(max-width: 1022px)" );
+
 class Carousel {
     constructor(carElement){
         this.element = carElement;
@@ -8,8 +10,8 @@ class Carousel {
         this.currentIndex = 0;
         this.cards = this.element.querySelectorAll('.card');
         this.cards[0].style.display = "flex";
-        this.cards[1].style.display = "flex";
-        this.cards[2].style.display = "flex";
+        this.cards[1].style.display = tablet.matches ? "none" : "flex";
+        this.cards[2].style.display = tablet.matches ? "none" : "flex";
         this.left.addEventListener('click', () => this.leftClick());
         this.right.addEventListener('click', () => this.rightClick());
     }
@@ -20,15 +22,16 @@ class Carousel {
         } else {
             this.currentIndex--;
         }
-        console.log(this.currentIndex);
         this.cards.forEach(x => x.style.display = 'none');
 
-        for(let i = 0; i < 3; i++){
-            let index = this.currentIndex + i >= this.numImgs ? this.currentIndex + i - 5 : this.currentIndex + i;
-            console.log('index', index, this.cards[index]);
-            this.cards[index].style.display = 'flex';  
-            this.cards[index].style.order = i;  
-
+        if(tablet.matches){
+            this.cards[this.currentIndex].style.display = 'flex';  
+        } else{
+            for(let i = 0; i < 3; i++){
+                let index = this.currentIndex + i >= this.numImgs ? this.currentIndex + i - 5 : this.currentIndex + i;
+                this.cards[index].style.display = 'flex';  
+                this.cards[index].style.order = i;  
+            }
         }
     }
 
@@ -41,12 +44,17 @@ class Carousel {
             this.currentIndex++;
         }
 
-        for(let i = 0; i < 3; i++){
-            let index = this.currentIndex + i >= this.numImgs ? this.currentIndex + i - 5 : this.currentIndex + i;
-            this.cards[index].style.display = 'flex';  
-            this.cards[index].style.order = i;  
-        }    
+        if(tablet.matches){
+            this.cards[this.currentIndex].style.display = 'flex';  
+        } else{
+            for(let i = 0; i < 3; i++){
+                let index = this.currentIndex + i >= this.numImgs ? this.currentIndex + i - 5 : this.currentIndex + i;
+                this.cards[index].style.display = 'flex';  
+                this.cards[index].style.order = i;  
+            }    
+        }
     }
 }
 
 let carousel = new Carousel(document.querySelector('.carousel'));
+
